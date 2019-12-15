7a3df822e530a092e1e365a2eba52972af68470f
騏瑋 劉 <xxxx6712@gmail.com>
2019-12-15 18:02:32 +0800

    新增欄位驗證控制項(Create頁面)

--- Comments/Models/Guestbook.cs
+++ Comments/Models/Guestbook.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Comments.Models
 {
@@ -10,10 +11,13 @@ namespace Comments.Models
     {
         public int Id { get; set; }
         [DisplayName("姓名")]
+        [Required]
         public string Name { get; set; }
         [DisplayName("電子信箱")]
+        [Required]
         public string Email { get; set; }
         [DisplayName("內容")]
+        [Required]
         public string Content { get; set; }
     }
 }
