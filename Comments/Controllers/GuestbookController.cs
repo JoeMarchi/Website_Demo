3c14e71dd33121703797c6d2626bf2c6ca0f9e8d
騏瑋 劉 <xxxx6712@gmail.com>
2020-01-07 18:58:06 +0800

    訪客留言網站ver.1.0

--- Comments/Controllers/GuestbookController.cs
+++ Comments/Controllers/GuestbookController.cs
@@ -14,39 +14,22 @@ namespace Comments.Controllers
     {
         private Website_DemoContext db = new Website_DemoContext();
 
-        // GET: Guestbook
+
         public ActionResult Index()
         {
             return View(db.Guestbooks.ToList());
         }
 
-        // GET: Guestbook/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Guestbook guestbook = db.Guestbooks.Find(id);
-            if (guestbook == null)
-            {
-                return HttpNotFound();
-            }
-            return View(guestbook);
-        }
 
-        // GET: Guestbook/Create
-        public ActionResult Create()
+        public ActionResult Write()
         {
             return View();
         }
 
-        // POST: Guestbook/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Email,Content")] Guestbook guestbook)
+        public ActionResult Write([Bind(Include = "Id,Name,Email,Content")] Guestbook guestbook)
         {
             if (ModelState.IsValid)
             {
@@ -58,56 +41,7 @@ namespace Comments.Controllers
             return View(guestbook);
         }
 
-        // GET: Guestbook/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Guestbook guestbook = db.Guestbooks.Find(id);
-            if (guestbook == null)
-            {
-                return HttpNotFound();
-            }
-            return View(guestbook);
-        }
-
-        // POST: Guestbook/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Email,Content")] Guestbook guestbook)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(guestbook).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(guestbook);
-        }
-
-        // GET: Guestbook/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Guestbook guestbook = db.Guestbooks.Find(id);
-            if (guestbook == null)
-            {
-                return HttpNotFound();
-            }
-            return View(guestbook);
-        }
-
-        // POST: Guestbook/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
             Guestbook guestbook = db.Guestbooks.Find(id);
             db.Guestbooks.Remove(guestbook);
---
+++ Comments/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Comments.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Comments.Models.Website_DemoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Comments.Models.Website_DemoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
